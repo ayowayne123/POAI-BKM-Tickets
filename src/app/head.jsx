35e8aab13e742a735e47103bca98ce1ba2e55faa
ayowayne123 <ayowayne123.com>
2023-01-28 11:12:19 +0200

    Base build

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>POAI - Live in BKM | Let our words speak II </title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Find our more about POAI 2nd Let our words speak show" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
