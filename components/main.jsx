b32fb6eaae37789e48a4e91d08ab6d5e6f466617
ayowayne123 <ayowayne123.com>
2023-02-01 14:22:31 +0200

    optimizing image

--- components/main.jsx
+++ components/main.jsx
@@ -6,9 +6,12 @@ import BkmMain from '../public/BkmMain.png'
 
 function Main() {
   return (
-    <div id='home' className='overflow-hidden h-screen'> 
+    <div id='home' className='overflow-hidden '> 
     <span className='text-7xl p-3 mx-60  uppercase font-bold  flex items-center justify-center text-center'> Cyprus&apos; biggest poetry event</span>
-<span className='uppercase absolute bottom-0 left-0 right-0 font-bold  flex items-center justify-center '><Image src={BkmMain} alt='Poets on an Island' width={750}/> </span>
+<span className='uppercase w-2/3 bottom-0 left-0 right-0 font-bold  flex items-center justify-center '>
+  <Image src={BkmMain} alt='Poets on an Island' width={900} height={900} sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"/> </span>
     </div>
   )
   
