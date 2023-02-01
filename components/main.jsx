9b666e16836e60fdccf25b00664cc21cf1ca0d9c
ayowayne123 <ayowayne123.com>
2023-02-01 14:30:34 +0200

    optimizing image 2

--- components/main.jsx
+++ components/main.jsx
@@ -8,8 +8,8 @@ function Main() {
   return (
     <div id='home' className='overflow-hidden '> 
     <span className='text-7xl p-3 mx-60  uppercase font-bold  flex items-center justify-center text-center'> Cyprus&apos; biggest poetry event</span>
-<span className='uppercase w-2/3 bottom-0 left-0 right-0 font-bold  flex items-center justify-center '>
-  <Image src={BkmMain} alt='Poets on an Island' width={900} height={900} sizes="(max-width: 768px) 100vw,
+<span className='uppercase w-full absolute bottom-0 left-0 right-0 font-bold  flex items-center justify-center '>
+  <Image src={BkmMain} alt='Poets on an Island' className='w-1/2' sizes="(max-width: 768px) 100vw,
               (max-width: 1200px) 50vw,
               33vw"/> </span>
     </div>
